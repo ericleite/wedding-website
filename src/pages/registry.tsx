67614cfeb8fe35723558a8d1648c09da59f8893b
Eric Leite <ericxleite@gmail.com>
2022-08-18 15:22:26 -0700

    add embedded zola registry

--- src/pages/registry.tsx
+++ src/pages/registry.tsx
@@ -1,38 +1,47 @@
-import { PageProps } from 'gatsby';
+import { PageProps, Script } from 'gatsby';
+import { OutboundLink } from 'gatsby-plugin-google-gtag';
 import { StaticImage } from 'gatsby-plugin-image';
-import React from 'react';
+import React, { useRef } from 'react';
 
 import * as globalStyles from '../assets/styles/global.module.css';
-import { Divider } from '../components';
 import { PageLayout, SectionContainer } from '../templates';
 import * as styles from './registry.module.css';
 
-const Registry: React.FC<PageProps> = () => (
-  <PageLayout
-    className="space-y-15 my-15"
-    heroImage={
-      <StaticImage
-        alt="Eric and Lauren sitting together on a ledge"
-        breakpoints={[1920, 2560, 3840]}
-        className={globalStyles.heroImage}
-        layout="fullWidth"
-        loading="eager"
-        placeholder="dominantColor"
-        quality={80}
-        src="../assets/images/heros/eric-and-lauren-sitting-together-on-ledge.jpg"
-      />
-    }
-    heroImageClassName={styles.heroImageContainer}
-    title="Registry"
-  >
-    <SectionContainer>
-      <section className="col-span-full flex flex-col items-center">
-        <h3 className="text-center">Stay tuned!</h3>
-        <Divider />
-        <p className="text-center">We&#39;ll be updating this page with more information soon!</p>
-      </section>
-    </SectionContainer>
-  </PageLayout>
-);
+const Registry: React.FC<PageProps> = () => {
+  const scriptId = useRef(Math.random().toString());
+  return (
+    <PageLayout
+      className="space-y-15 my-15"
+      heroImage={
+        <StaticImage
+          alt="Eric and Lauren sitting together on a ledge"
+          breakpoints={[1920, 2560, 3840]}
+          className={globalStyles.heroImage}
+          layout="fullWidth"
+          loading="eager"
+          placeholder="dominantColor"
+          quality={80}
+          src="../assets/images/heros/eric-and-lauren-sitting-together-on-ledge.jpg"
+        />
+      }
+      heroImageClassName={styles.heroImageContainer}
+      title="Registry"
+    >
+      <SectionContainer>
+        <section className="col-span-full flex flex-col items-center">
+          <OutboundLink
+            className="zola-registry-embed"
+            data-registry-key="eric-and-lauren"
+            href="https://www.zola.com/registry/eric-and-lauren"
+            target="_blank"
+          >
+            Click here to see our Wedding Registry
+          </OutboundLink>
+          <Script id={scriptId.current} src="https://widget.zola.com/js/widget.js" />
+        </section>
+      </SectionContainer>
+    </PageLayout>
+  );
+};
 
 export default Registry;
