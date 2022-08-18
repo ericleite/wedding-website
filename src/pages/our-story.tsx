6148110873bf2b39c99d8865f98b13d1995335a3
Eric Leite <ericxleite@gmail.com>
2022-08-18 15:22:26 -0700

    add details to our story page

--- src/pages/our-story.tsx
+++ src/pages/our-story.tsx
@@ -44,6 +44,13 @@ const OurStory: React.FC<PageProps> = () => {
           <div className="hidden 2xl:block absolute top-12 left-1/2 bottom-0 -translate-x-1/2 w-2 bg-lightSecondary" />
           <div className="flex flex-col space-y-13 lg:space-y-15">
             <TimelineSection
+              body={
+                <p>
+                  Eric met Lauren while she was dressed as Waldo for her big-little sorority reveal. They started
+                  talking, and he asked her out on a date. Neither knew this night would be the start of their adventure
+                  together.
+                </p>
+              }
               date="September 2013"
               image={
                 <StaticImage
@@ -57,10 +64,16 @@ const OurStory: React.FC<PageProps> = () => {
               title="A boy found &#8220;Waldo&#8221;"
             />
             <TimelineSection
+              body={
+                <p>
+                  After going on movie and dinner dates almost every day, they knew they had something special. So it
+                  wasn&#39;t long before they decided to make it official.
+                </p>
+              }
               date="October 2013"
               image={
                 <StaticImage
-                  alt="Eric and Lauren laughing during a photo shoot"
+                  alt="Eric and Lauren laughing while taking photos before a date function"
                   aspectRatio={16 / 9}
                   layout="fullWidth"
                   quality={80}
@@ -74,6 +87,13 @@ const OurStory: React.FC<PageProps> = () => {
               title="They became #FacebookOfficial"
             />
             <TimelineSection
+              body={
+                <p>
+                  Their relationship grew stronger over the years while attending the University of Florida together.
+                  Some of their favorite moments were cheering at Gator games, taking road trips, going on date
+                  functions, and hiking the trails around Gainesville.
+                </p>
+              }
               date="May 2015"
               image={
                 <StaticImage
@@ -90,6 +110,13 @@ const OurStory: React.FC<PageProps> = () => {
               title="And lived their best lives at UF"
             />
             <TimelineSection
+              body={
+                <p>
+                  After graduating, Eric got a job offer in California. Lauren was still studying, so they had to make
+                  the most challenging decision in their life: to enter a long-distance relationship. They loved each
+                  other very much, so they did whatever it took to make it work.
+                </p>
+              }
               date="November 2016"
               image={
                 <StaticImage
@@ -104,6 +131,13 @@ const OurStory: React.FC<PageProps> = () => {
               title="2,500 miles couldn&#39;t keep them apart"
             />
             <TimelineSection
+              body={
+                <p>
+                  After three long years apart, Lauren moved to San Francisco to be with Eric. They lived together for
+                  the first time and ended up adopting Eve, their first dog together. The three of them finally felt at
+                  home.
+                </p>
+              }
               date="September 2019"
               image={
                 <StaticImage
@@ -117,9 +151,16 @@ const OurStory: React.FC<PageProps> = () => {
                   }}
                 />
               }
-              title="Eventually, they found home in California"
+              title="Eventually, they found home"
             />
             <TimelineSection
+              body={
+                <p>
+                  Just months after Lauren moved to SF, the pandemic started. Eric and Lauren were grateful to have each
+                  other, and they found new ways to make their favorite meals together, played video games, and taught
+                  Eve some tricks. Despite the hectic outside world, they had their own space just for them.
+                </p>
+              }
               date="May 2020"
               image={
                 <StaticImage
@@ -134,19 +175,33 @@ const OurStory: React.FC<PageProps> = () => {
               title="Just in time for a global pandemic!"
             />
             <TimelineSection
+              body={
+                <p>
+                  They tried to live a &#8220;normal&#8221; life by going outdoors as much as they could. From bike
+                  rides around the city to Disneyland adventures, they became closer every day. In one short year, they
+                  made new friends and unforgettable memories together.
+                </p>
+              }
               date="August 2021"
               image={
                 <StaticImage
-                  alt="Eric and Lauren at Coachella"
+                  alt="Eric and Lauren with friends at Disneyland"
                   aspectRatio={16 / 9}
                   layout="fullWidth"
                   quality={80}
                   src="../assets/images/moments/eric-lauren-and-friends-at-disneyland.jpg"
                 />
               }
-              title="But that didn&#39;t stop them from having a little fun"
+              title="But that didn&#39;t stop them from going on adventures"
             />
             <TimelineSection
+              body={
+                <p>
+                  For their anniversary, they went to Hawaii to surf, star gaze on top of a volcano, and hike through
+                  waterfalls. Then, the day before their eighth anniversary, Eric organized a photoshoot where he got
+                  down on one knee and popped the question.
+                </p>
+              }
               date="October 2021"
               image={
                 <StaticImage
